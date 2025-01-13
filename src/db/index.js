7bd53cbb932358921bf5e19650e12058eb440300
Soham <sohamholkar800@gmail.com>
2025-01-13 22:07:54 +0530

    Added the Project Structure

---
+++ src/db/index.js
@@ -0,0 +1,22 @@
+/**
+ * This file is containing the logic for the database connection . . .
+*/
+
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+import dotenv from "dotenv" ;
+dotenv.config({path : "../.env"}) ;
+
+const connectDB = async () => {
+    try{
+        console.log("Current directory:", process.cwd());
+        console.log(process.env.MONGODB_URI);
+        const connectionInstance = await mongoose.connect(process.env.MONGODB_URI)
+        console.log(`\n mongodb connected `);
+    }catch(err){
+        console.log("MongoDB Connection Error . . . ") ;
+        console.log(err);
+    }
+}
+
+export default connectDB ;
