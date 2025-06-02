d7fb6ceebb812829ec45a6d53e04597b7176b0f2
Soham <sohamholkar800@gmail.com>
2025-06-02 16:46:01 +0530

    solved the error

--- src/routes/mentor.routes.js
+++ src/routes/mentor.routes.js
@@ -8,12 +8,12 @@ router.route("/get-all-mentors").get(getAllMentors);
 router.route("/add-mentor").post(verifyTokenOfAdmin, authorizeRole(['Admin']) , addMentor);                  // Create a new blog topic
 router.route("/update-mentor-by-id/:id").put(verifyTokenOfAdmin, authorizeRole(['Admin']) ,updateMentor);                // Update a blog topic by ID
 // router.route("/delete-blog/:id").delete(verifyTokenOfAdmin , authorizeRole(['Admin']), deleteTopic);             // Delete a blog topic by ID
-router.route("/add-blog/:id/addBlog").post(verifyTokenOfAdmin , authorizeRole(['Admin']),addBlogToTopic);    // Add a blog to a topic
+// router.route("/add-blog/:id/addBlog").post(verifyTokenOfAdmin , authorizeRole(['Admin']),addBlogToTopic);    // Add a blog to a topic
 
-router.route("/:id/blogs").get(getBlogsByTopic);        // Get all blogs under a topic
-router.route("/:topicId/blog/:blogId").get(getSpecificBlog); // Get a specific blog inside a topic
-router.route("/:topicId/blog/:blogId").put(updateBlog); // Update a blog inside a topic
-router.route("/:topicId/blog/:blogId").delete(deleteBlog); 
+// router.route("/:id/blogs").get(getBlogsByTopic);        // Get all blogs under a topic
+// router.route("/:topicId/blog/:blogId").get(getSpecificBlog); // Get a specific blog inside a topic
+// router.route("/:topicId/blog/:blogId").put(updateBlog); // Update a blog inside a topic
+// router.route("/:topicId/blog/:blogId").delete(deleteBlog); 
 
 
 export default router ;
