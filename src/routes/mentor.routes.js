bd5cee5e0ef5f35a374dddfd33f691470e176a10
Soham <sohamholkar800@gmail.com>
2025-06-02 16:40:26 +0530

    solved the error

--- src/routes/mentor.routes.js
+++ src/routes/mentor.routes.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.route("/get-all-mentors").get(getAllMentors);                   
 router.route("/add-mentor").post(verifyTokenOfAdmin, authorizeRole(['Admin']) , addMentor);                  // Create a new blog topic
 router.route("/update-mentor-by-id/:id").put(verifyTokenOfAdmin, authorizeRole(['Admin']) ,updateMentor);                // Update a blog topic by ID
-router.route("/delete-blog/:id").delete(verifyTokenOfAdmin , authorizeRole(['Admin']), deleteTopic);             // Delete a blog topic by ID
+// router.route("/delete-blog/:id").delete(verifyTokenOfAdmin , authorizeRole(['Admin']), deleteTopic);             // Delete a blog topic by ID
 router.route("/add-blog/:id/addBlog").post(verifyTokenOfAdmin , authorizeRole(['Admin']),addBlogToTopic);    // Add a blog to a topic
 
 router.route("/:id/blogs").get(getBlogsByTopic);        // Get all blogs under a topic
